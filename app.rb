fbd90fe1022ded115a010c6c8339d7431aeeec34
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-13 03:29:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,21 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+
+    if @operation = "add"
+      "#{@number1 + @number2}"
+    elsif @operation = "subtract"
+      "#{@number1 - @number2}"
+    elsif @operation = "divide"
+      "#{@number1 / @number2}"
+    elsif @operation = "multiply"
+      "#{@number1 * @number2}"
+    else
+      "You did not choose an operation."
+    end
   end
 
 end
