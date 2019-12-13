e9f5270ea6e5ad8467563eb1648229b62db5b840
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-13 02:43:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    binding.pry
+  end
 
 end
