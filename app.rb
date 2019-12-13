649faf54c23a23d0bb65c022c5a28d5f38b4bdd2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-13 04:46:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -41,6 +41,12 @@ class App < Sinatra::Base
     case @operation
     when "add"
       "#{@num1 + @num2}"
+    when "subtract"
+      "#{@num1 - @num2}"
+    when "divide"
+      "#{@num1 / @num2}"
+    when "multiply"
+      "#{@num1 * @num2}"
     end
   end
 
