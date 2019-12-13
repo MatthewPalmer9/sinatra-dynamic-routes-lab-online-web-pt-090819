2e3e1c33d59c39eb29d7530c92ab43eb16e6ac53
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-13 03:21:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
     @word_array = [@word1, @word2, @word3, @word4, @word5]
-    "#{@word_array.join(" ")}"
+    "#{@word_array.join(" ")}."
   end
 
 end
