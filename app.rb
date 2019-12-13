542397193f68145a7535d44aabb566d59a3743e2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-13 03:07:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_a
     @phrase = params[:phrase]
+    binding.pry
     "#{@number.each do @phrase end}"
   end
 
